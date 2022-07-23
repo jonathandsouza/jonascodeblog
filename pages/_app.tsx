58a348370b961da4f4b319486b82cc6ac8d60ca5
Jonathan DSouza <j.dsouza@freetimecompany.nl>
2022-07-23 12:38:52 +0200

    added GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Jona&pos;s code blog</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+			<Script src="https://www.googletagmanager.com/gtm.js?id=GTM-WF222HH" />
+		</>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
 const Home: NextPage = () => {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center py-2">
-			<Head>
-				<title>Jona&pos;s code blog</title>
-				<link rel="icon" href="/favicon.ico" />
-				<meta
-					name="google-site-verification"
-					content="Wl7qilM3OM9c8pqDwDuqLnpbe7WWm5OZhKoW0TMvJQk"
-				/>
-			</Head>
-
 			<code>Today I start writing my blog </code>
 		</div>
 	);
