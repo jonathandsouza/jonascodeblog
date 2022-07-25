d1105504b7db65f9ec54341a1c118650f543950e
Jonathan DSouza <j.dsouza@freetimecompany.nl>
2022-07-25 19:08:55 +0200

    added my first post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
 	return (
-		<div className="container flex min-h-screen flex-col items-center justify-center py-2">
-			<code>Today I start writing my blog </code>
+		<div className="container">
+			<h1 className="text-3xl font-bold pt-10 text-blue-700">Posts</h1>
+			<h2 className="text-2xl text-blue-500 pt-5">
+				<Link href={"/puppeteer-with-nest"}>
+					<a>
+						1. Implementation for a Prerender service using
+						Puppeteer + NestJS
+					</a>
+				</Link>
+			</h2>
 		</div>
 	);
 };
---
+++ pages/puppeteer-with-nest.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+	return (
+		<div className="container">
+			<article>
+				<h1>
+					Implementing a pre-render service using Puppeteer & NestJS
+				</h1>
+
+				<p>github:</p>
+
+				<h2>table of contents</h2>
+				<ul>
+					<li>What is a Pre-render service?</li>
+					<li>Why do you need a Pre-render service?</li>
+					<li>Tools & libs, we will be using to build the service</li>
+				</ul>
+
+				<h2>What is a Pre-render service?</h2>
+				<p></p>
+			</article>
+		</div>
+	);
+};
+
+export default Home;
