b99c1037ed50ae6d771908ba7722c8836d004e76
j.dsouza <j.dsouza@freetimecompany.nl>
2022-09-08 23:05:07 +0200

    styling improvments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,13 +13,53 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<header>
-				<nav className="container flex justify-end py-5 ">
-					<Link href="/">
-						<a className="text-2xl font-bold">Posts</a>
-					</Link>
-				</nav>
+				<div className="container mx-auto max-w-screen-lg px-3 py-6">
+					<div className="flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between">
+						<Link href="/">
+							<a>
+								<div
+									className="bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-4xl font-bold text-transparent"
+									style={{ lineHeight: "3.5rem" }}
+								>
+									Jona&apos;s Blog
+								</div>
+							</a>
+						</Link>
+						<nav>
+							<ul className="flex gap-x-3 font-medium text-gray-200 text-xl">
+								<li className="hover:text-white">
+									<a
+										href="https://stackoverflow.com/users/3051279/jonathan-dsouza"
+										target="_blank"
+										rel="noreferrer"
+									>
+										Stack Overflow
+									</a>
+								</li>
+								<li className="hover:text-white">
+									<a
+										href="https://github.com/jonathandsouza"
+										target="_blank"
+										rel="noreferrer"
+									>
+										GitHub
+									</a>
+								</li>
+								<li className="hover:text-white">
+									<a
+										href="https://twitter.com/JonathanDsouza3"
+										target="_blank"
+										rel="noreferrer"
+									>
+										Twitter
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
 			</header>
-			<main className="bg-slate-900 text-gray-100">
+			<main>
 				<Component {...pageProps} />
 			</main>
 			<Script src="https://www.googletagmanager.com/gtm.js?id=GTM-WF222HH" />
