f444ee5536c880ccb90266d0e6e115a70304d506
j.dsouza <j.dsouza@freetimecompany.nl>
2023-02-14 12:46:37 +0530

    added document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,11 @@ export default function Document() {
 			<body className="bg-slate-900 text-gray-100">
 				<Main />
 				<NextScript />
+				<script
+					async
+					src="https://cdn.requestmetrics.com/agent/current/rm.js"
+					data-rm-token="z6nv8qk:q2ct3bc"
+				></script>
 			</body>
 		</Html>
 	);
